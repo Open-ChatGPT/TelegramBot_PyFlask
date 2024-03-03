aab1025abf6caf8bd7359c46b313b4dc923f48e3
Richard Evans <103039975+Open-ChatGPT@users.noreply.github.com>
2024-03-04 07:58:12 +0800

    更新 TelegramBot.py

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,7 +1,15 @@
 # TelegramBot.py
-from telegram import ForceReply, Update
-from telegram.ext import CommandHandler, ContextTypes, MessageHandler, filters
-import MySQL
+import logging
+from telegram import ForceReply, Update, BotCommand
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+import MySQL #引用MySQL文件
+
+# 启用日志记录
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_user
@@ -14,7 +22,10 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(update.message.text)
 
+# 设置命令和初始化Telegram机器人
 def setup(application):
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
+    
+    application.add_handler(CommandHandler("show_chats", show_chats))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
