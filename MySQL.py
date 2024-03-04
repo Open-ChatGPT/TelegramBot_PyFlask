853d79a6b409a31902b5df8e1ab7f3218d539df6
Richard Evans <103039975+Open-ChatGPT@users.noreply.github.com>
2024-03-04 08:44:30 +0800

    更新 MySQL.py

--- MySQL.py
+++ MySQL.py
@@ -20,3 +20,14 @@ def insert_group_message(group_id, group_name, user_id, user_name, message_text)
         db_connection.commit()
     except mysql.connector.Error as err:
         print("Error inserting group message: {}".format(err))
+
+def check_url_exists(group_id, url):
+    try:
+        with db_connection.cursor() as cursor:
+            query = "SELECT EXISTS(SELECT 1 FROM db_chat WHERE groupid=%s AND url=%s)"
+            cursor.execute(query, (group_id, url))
+            result = cursor.fetchone()
+            return result[0] == 1  # 返回布尔值，表示是否存在
+    except mysql.connector.Error as err:
+        print(f"Error checking URL existence: {err}")
+        return False
